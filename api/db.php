1aadd60c39757657ba7454302bb3324398667e77
srisomesh-ai <somesh9346220090@gmail.com>
2026-08-17 11:58:25 +0530

    fix: add X-Admin-Key to CORS allowed headers

--- api/db.php
+++ api/db.php
@@ -35,7 +35,7 @@ function getDB() {
 function setCorsHeaders() {
     header('Access-Control-Allow-Origin: *');
     header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-    header('Access-Control-Allow-Headers: Content-Type, Authorization');
+    header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Admin-Key');
     header('Content-Type: application/json; charset=utf-8');
     if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
         http_response_code(200); exit;
